12f1f80927bde9141b624ff5c87aa1f461bc2f59
RizkiIqbal02 <roxashiqbal12@gmail.com>
2023-12-28 18:17:58 +0700

    benerin cara require

--- coba.js
+++ coba.js
@@ -1,4 +1,6 @@
 const cetakNama = (nama) => `Hai ${nama}, Selamat ngoding`;
+const umur = 21;
 
 
-module.exports = cetakNama;
+module.exports.cetakNama = cetakNama;
+module.exports.umur = umur;
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const cetakNama = require('./coba');
+// Seharusnya module ini dinamain sesuai nama File nya, bukan nama function nya
+const coba = require('./coba');
 
-console.log(cetakNama('iqbal'));
+console.log(coba);
+// console.log(cetakNama('iqbal'));
